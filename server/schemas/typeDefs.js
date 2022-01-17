01227d56e262ff073d64c3250408eba01153e0b0
Jamie Williams <jamwil1226@yahoo.com>
2022-01-17 14:57:43 -0500

    set up Apollo

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,45 @@
+// import the gql tagged template function
+const { gql } = require('apollo-server-express');
+
+// create our typeDefs
+const typeDefs = gql`
+  type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(): User
+    removeBook(_ID: INT): User
+  }
+
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: ID
+    authors: [String]
+    description: String
+    title: String
+    image: 
+    link: 
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+
+`;
+
+// export the typeDefs
+module.exports = typeDefs;
